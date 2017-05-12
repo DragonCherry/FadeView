ec7cdfefdf0cd67c96f9184a5c86198700373a6e
DragonCherry <dragoncherry@naver.com>
2017-05-12 13:56:43 +0900

    .

---
+++ FadeView/Classes/UIView+Fade.swift
@@ -0,0 +1,43 @@
+//
+//  UIView+Fade.swift
+//  Pods
+//
+//  Created by DragonCherry on 5/12/17.
+//
+//
+
+import UIKit
+
+extension UIView {
+    
+    public func fadeOutFromSuperview(_ duration: TimeInterval = 0.25, completion: (() -> Void)? = nil) {
+        if let _ = self.superview {
+            UIView.animate(
+                withDuration: duration,
+                animations: {
+                    self.alpha = 0
+            },
+                completion: { finished in
+                    self.isHidden = true
+                    self.removeFromSuperview()
+                    completion?()
+            })
+        }
+    }
+    
+    public func fadeInSubview(_ subview: UIView, duration: TimeInterval = 0.25, completion: (() -> Void)? = nil) {
+        if self.superview == nil {
+            addSubview(subview)
+            isHidden = false
+            self.alpha = 0
+            UIView.animate(
+                withDuration: duration,
+                animations: {
+                    self.alpha = 1
+            },
+                completion: { finished in
+                    completion?()
+            })
+        }
+    }
+}
